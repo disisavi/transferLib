68cf60176acf9eb30c01c6100ef7b4257bb07535
Abhijeet Mishra <dsisavi@gmail.com>
2020-04-20 01:29:52 -0400

    Login successfull. Some refactoring done as well. I need to now access playlist api

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-from typing import Dict
 from threading import Timer
-import requests
 import utils as ut
-from flask import Flask,redirect
+import requests
+from flask import Flask, redirect, request
 import webbrowser
 
 
@@ -10,21 +9,32 @@ def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
 
-
     def open_browser():
         webbrowser.open_new('http://127.0.0.1:5000/')
 
     @app.route('/')
-    def appSetup():    
-        spotifyURL = ut.getURI()
-        param = ut.createParams(['client_id', 'redirect_uri', 'response_type'])
-        return redirect(ut.createRedirectURI(spotifyURL,param))    
+    def appSetup():
+        spotifyURI = ut.getAuthURI()
+        param = ut.createParams(['client_id', 'redirect_uri', 'response_type', 'state'])
+        return redirect(ut.createRedirectURI(spotifyURI, param))
 
-    @app.route('/redirect')
-    def res():
-        print("Called")
+    @app.route('/redirect', methods=['GET'])
+    def redirectSpotify():
+        response = request.args
+        state = response['state']
 
+        if state != ut.getState():
+            raise ValueError("State does not match. Incorrect request")
+        if 'error' in response:
+            raise Exception(response['error'])
+
+        ut.setAuthCode(response['code'])
+        param = ut.createParams(['grant_type', 'code', 'redirect_uri'])
+        header = ut.createHeader()
+        uri = ut.getTokenURI()
+        spotifyResponse = requests.post(uri, data=param, headers=header)
+        print(spotifyResponse.text)
+        return "Hello Bitches"
 
     Timer(1, open_browser).start();
     return app
-
--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 from typing import Dict
 from base64 import b64encode
 
-clidentDetails = {}
+clientDetails = {}
 delimeter = "="
 
 
@@ -10,7 +10,7 @@ def createParams(listOfParameters):
     for param in listOfParameters:
         try:
             key = param
-            value = clidentDetails[param]
+            value = clientDetails.get(param, None)
             paramDict[key] = value
         except KeyError:
             print(" Key " + key + " not found..")
@@ -18,8 +18,50 @@ def createParams(listOfParameters):
     return paramDict
 
 
-def getURI():
-    return clidentDetails["spotifyURI"]
+def createHeader():
+    header = {}
+    key = 'Authorization'
+    valueToEncode = clientDetails['client_id'] + ':' + clientDetails['client_secret']
+    value = "Basic ".encode('utf-8') + b64encode(valueToEncode.encode('utf-8'))
+    header[key] = value
+
+    return header
+
+
+def getAuthURI():
+    return clientDetails["spotifyURI"]
+
+
+def getTokenURI():
+    return "https://accounts.spotify.com/api/token"
+
+
+def getState():
+    return clientDetails.get('state', None)
+
+
+def setAuthCode(code):
+    clientDetails['code'] = code
+
+
+def createRedirectURI(uri: str, params: Dict):
+    redirectURI = uri + '?'
+    for key, value in params.items():
+
+        if isinstance(value, str):
+            redirectURI = redirectURI + key + '=' + value + '&'
+
+        if isinstance(value, list):
+            redirectURI = redirectURI + key
+            for things in value:
+                redirectURI = redirectURI + ' ' + things
+
+            redirectURI = redirectURI + '&'
+
+    if redirectURI[-1] == '&':
+        redirectURI = redirectURI[:-1]
+
+    return redirectURI
 
 
 with open('dev.properties', 'r') as propFile:
@@ -30,19 +72,4 @@ with open('dev.properties', 'r') as propFile:
         lineSplit = line.replace(" ", "").replace("\n", "").split(delimeter)
         if len(lineSplit) != 2:
             raise Exception("Invalid File")
-        clidentDetails[lineSplit[0]] = lineSplit[1]
-
-def createRedirectURI(url:str, params:Dict):
-    redirectURI = url+'?'
-    for key, value in params.items():
-        if isinstance(value,str):
-            redirectURI = redirectURI +key+'='+value+'&'
-        if isinstance(value, list):
-            # for now
-            pass
-    print("D1 "+redirectURI)
-    if redirectURI[-1] == '&':
-        print("D2 "+redirectURI[:-2])
-        redirectURI = redirectURI[:-1]
-    
-    return redirectURI
+        clientDetails[lineSplit[0]] = lineSplit[1]
