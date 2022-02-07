47c7541b18b86455132c3482aba580679ada519c
Abhijeet Mishra <dsisavi@gmail.com>
2022-02-06 23:11:13 -0500

    Reverted to flask model becuase Client Credentials flow doesn provide with user info (like playlists)

--- app.py
+++ app.py
@@ -1,18 +1,83 @@
+from threading import Timer
+import utils as ut
 import requests
+from flask import Flask, redirect, request, render_template
+import webbrowser
+from SpotifyPlaylist import SpotifyPlaylist
+from typing import Dict
 
-import util
-from util import load_props, createParams, createHeader
-
-
-def main():
-    load_props()
-    params = createHeader()
-    param = createParams(['grant_type', "scope"])
-    print(params, param)
-    response = requests.post(util.getAuthURI(), headers=params, data=param)
-    print(response.text)
-    response.raise_for_status()
+# create and configure the app
+app = Flask(__name__)
+print(__name__)
 
 
-if __name__ == "__main__":
-    main()
+def open_browser():
+    webbrowser.open_new('http://127.0.0.1:5000/')
+
+
+@app.route('/redirect_to_spotify')
+def appSetup():
+    """Entry point for the app, duh"""
+    print("In here")
+    spotifyURI = ut.getAuthURI()
+    param = ut.createParams(['client_id', 'redirect_uri', 'response_type', 'scope', 'state'])
+    return redirect(ut.createRedirectURI(spotifyURI, param))
+
+
+@app.route('/redirect', methods=['GET'])
+def redirectSpotify():
+    response = request.args
+    state = response['state']
+    if state != ut.getState():
+        raise ValueError("State does not match. Incorrect request")
+    if 'error' in response:
+        raise Exception(response['error'])
+
+    ut.setAuthCode(response['code'])
+    param = ut.createParams(['grant_type', 'code', 'redirect_uri'])
+    header = ut.createHeader()
+
+    uri = ut.getTokenURI()
+    spotifyResponse = requests.post(uri, data=param, headers=header)
+    ut.setSpotifyUserToken(spotifyResponse.json())
+    return render_template("login.html")
+
+
+@app.route("/getPlaylist")
+def getPlaylists():
+    spotify_playlist_uri = ut.getPlaylistURI()
+    header = ut.createParams(['Authorization'], True)
+    header['Authorization'] = 'Bearer ' + header['Authorization']
+    playlist_response = requests.get(spotify_playlist_uri, headers=header)
+    spotify_playlist_list: Dict = playlist_response.json()['items']
+    list_playlist = []
+
+    for ind, spotify_playlist in enumerate(spotify_playlist_list):
+        playlist = SpotifyPlaylist(spotify_playlist['name'])
+        list_playlist.append(playlist)
+
+        playlist_uri = spotify_playlist.get('tracks', None).get('href', None)
+        tracks_response_object = requests.get(playlist_uri, headers=header)
+        tracks_response_object = tracks_response_object.json()
+
+        for idx, tracks_responce in enumerate(tracks_response_object['items']):
+            track_object = tracks_responce['track']
+            if track_object is None:
+                continue
+            artist_names = []
+            name_track = track_object['name']
+            for artist in track_object['artists']:
+                artist_names.append(artist['name'])
+
+            playlist.addToPlaylist(name_track, artist_names)
+    ut.write_playlist_file(list_playlist)
+    return "FeelGood"
+
+
+if __name__ == '__main__':
+    # open_browser()
+    app.run(debug=True)
+# TODO
+#     1. Please refactor the code. Get playlist is a mess
+#     2. Create a proper Conda Env
+#     3. Start integrating youtube API
